1ecee8f11e3c376bc075f3e45f5841acfe36354b
FelBenini <112193194+FelBenini@users.noreply.github.com>
2022-12-31 00:09:52 -0300

    modified:   src/pages/homepage.js 	modified:   src/style/App.css 	modified:   src/style/App.css.map 	modified:   src/style/App.scss

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -18,14 +18,22 @@ function Homepage() {
     const cryptoMap = cryptoCoins.map((coin) => {
         return (
             <div className="coin-listed">
+                <span>
                 <img src={coin?.image} alt={coin}/>
                 <h4>{coin.name}</h4>
-                <p>{currencyPrefix}{coin.current_price}</p>
+                </span>
+                <span>
+                    <h5>{coin.price_change_percentage_24h + "%"}</h5>
+                </span>
+                <span id="price-crypto">
+                <p>Price:</p>
+                <h5>{currencyPrefix}{coin.current_price.toFixed(2)}</h5>
+                </span>
             </div>
         )
     })
   return (
-    <div>
+    <div id="coins-display">
         {cryptoMap}
     </div>
   )
