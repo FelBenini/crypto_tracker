770d3c3b8e7f6dc1390e7d2cedb6c4c065bfa4fc
FelBenini <112193194+FelBenini@users.noreply.github.com>
2023-01-02 21:48:39 -0300

    modified:   src/components/mobile-menu.js 	modified:   src/pages/searchPage.js

--- src/components/mobile-menu.js
+++ src/components/mobile-menu.js
@@ -5,22 +5,35 @@ import { RadioMenu } from './side-menu';
 import { IconButton } from '@mui/material';
 
 function MobileMenu() {
-    function handleMobileMenu() {
-        document.getElementById('mobile-menu').style.transform = "translateX(100%)"
-        setTimeout(function() {
-            document.getElementById('mobile-menu').style.display = "none";
-        }, 300);
-    }
+  function handleMobileMenu() {
+    document.getElementById('mobile-menu').style.transform = "translateX(100%)"
+    setTimeout(function () {
+      document.getElementById('mobile-menu').style.display = "none";
+    }, 300);
+  }
   return (
     <div id="mobile-menu">
-        <IconButton sx={{marginRight: "72%"}} color="primary" size="medium" onClick={handleMobileMenu}>
-        <img src="/img/mobile-menu-exit.svg" alt="close mobile menu"/>
+      <IconButton sx={{ marginRight: "72%" }} color="primary" size="medium" onClick={handleMobileMenu}>
+        <img src="/img/mobile-menu-exit.svg" alt="close mobile menu" />
+      </IconButton>
+      <SearchBar />
+      <h3>Currency</h3>
+      <FormSelect />
+      <h3>Order by</h3>
+      <RadioMenu />
+      <span>
+        <IconButton color="primary" size="medium" id="dark-switcher">
+          <input hidden accept="image/*" />
+          <img src="/img/theme-mode-dark.svg" alt="dark theme switcher" />
         </IconButton>
-        <SearchBar/>
-        <h3>Currency</h3>
-        <FormSelect/>
-        <h3>Order by</h3>
-        <RadioMenu/>
+
+        <a href="https://github.com/FelBenini/crypto_tracker" target="blank">
+          <IconButton color="primary" size="medium" id="github-link">
+            <input hidden accept="image/*" />
+            <img src="/img/github-icon.svg" alt="github code button link" />
+          </IconButton>
+        </a>
+      </span>
     </div>
   )
 }
--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -6,7 +6,7 @@ import { formattedNumber } from './homepage';
 
 function SearchPage() {
     const { query } = useParams()
-    const { currency, currencyPrefix} = CurrencyState()
+    const { currency, currencyPrefix } = CurrencyState()
     const [cryptoCoins, setCryptoCoins] = useState([])
     const filteredObjects = cryptoCoins.filter(item => item.name.toLowerCase().indexOf(query.toLowerCase()) === 0);
     const fetchCoins = async (bid) => {
@@ -15,41 +15,42 @@ function SearchPage() {
         setCryptoCoins(data)
     }
     const mappedObjects = filteredObjects.map(coin => {
-    if(coin.price_change_percentage_24h > 0) {
-        return (
-            <div className="coin-listed">
-                <span>
-                <img src={coin?.image} alt={coin} className="coin-image"/>
-                <h4>{coin.name}</h4>
-                </span>
-                <span className="variation-24h">
-                    <img src="/img/variation-positive.svg" alt="trending down"/><h5>{coin.price_change_percentage_24h.toFixed(2) + "%"}</h5>
-                </span>
-                <span className="price-crypto">
-                <p>Price:</p>
-                <h5>{currencyPrefix}{formattedNumber(coin.current_price.toFixed(2))}</h5>
-                </span>
-            </div>
-        )} else if(coin.price_change_percentage_24h < 0) {
+        if (coin.price_change_percentage_24h > 0) {
             return (
                 <div className="coin-listed">
-                <span>
-                <img src={coin.image} alt={coin} className="coin-image"/>
-                <h4>{coin.name}</h4>
-                </span>
-                <span className="variation-24h">
-                    <img src="/img/variation-negative.svg" alt="trending down"/><h5>{coin.price_change_percentage_24h.toFixed(2) + "%"}</h5>
-                </span>
-                <span className="price-crypto">
-                <p>Price:</p>
-                <h5>{currencyPrefix}{formattedNumber(coin.current_price.toFixed(2))}</h5>
-                </span>
-            </div>
+                    <span>
+                        <img src={coin?.image} alt={coin} className="coin-image" />
+                        <h4>{coin.name}</h4>
+                    </span>
+                    <span className="variation-24h">
+                        <img src="/img/variation-positive.svg" alt="trending down" /><h5>{coin.price_change_percentage_24h.toFixed(2) + "%"}</h5>
+                    </span>
+                    <span className="price-crypto">
+                        <p>Price:</p>
+                        <h5>{currencyPrefix}{formattedNumber(coin.current_price.toFixed(2))}</h5>
+                    </span>
+                </div>
+            )
+        } else if (coin.price_change_percentage_24h < 0) {
+            return (
+                <div className="coin-listed">
+                    <span>
+                        <img src={coin.image} alt={coin} className="coin-image" />
+                        <h4>{coin.name}</h4>
+                    </span>
+                    <span className="variation-24h">
+                        <img src="/img/variation-negative.svg" alt="trending down" /><h5>{coin.price_change_percentage_24h.toFixed(2) + "%"}</h5>
+                    </span>
+                    <span className="price-crypto">
+                        <p>Price:</p>
+                        <h5>{currencyPrefix}{formattedNumber(coin.current_price.toFixed(2))}</h5>
+                    </span>
+                </div>
             )
         }
-      ;
-  });
-      useEffect(() => {
+        ;
+    });
+    useEffect(() => {
         fetchCoins(currency)
     }, [currency])
     const headerStyle = {
@@ -59,14 +60,14 @@ function SearchPage() {
     return (
         <section>
             <h2 style={headerStyle}>Showing results for {query}</h2>
-        <div id="search-section">
-            <div className="coin-label">
-                <span>Name</span>
-                <span className="variation-24h">Variation 24h</span>
-                <span className='price-crypto'>Price</span>
+            <div id="search-section">
+                <div className="coin-label">
+                    <span>Name</span>
+                    <span className="variation-24h">Variation 24h</span>
+                    <span className='price-crypto'>Price</span>
+                </div>
+                {mappedObjects}
             </div>
-            {mappedObjects}
-        </div>
         </section>
 
     )
