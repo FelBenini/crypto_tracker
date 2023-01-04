821a82f153cc13a499383b87f842d68d3f364abe
FelBenini <112193194+FelBenini@users.noreply.github.com>
2023-01-04 18:14:05 -0300

    modified:   src/pages/coinInfo.js

--- src/pages/coinInfo.js
+++ src/pages/coinInfo.js
@@ -4,27 +4,36 @@ import { Line } from "react-chartjs-2";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { CurrencyState } from '../currencyContext';
+import { Chart as ChartJS, LineElement, PointElement, LinearScale, Title } from 'chart.js';
 
 function CoinPage() {
-    const {currency} = CurrencyState()
-    const {id} = useParams()
-    const [chartData, setChartData] = useState([])
+  const { currency } = CurrencyState()
+  const { id } = useParams()
+  const [chartData, setChartData] = useState([])
 
-    const getData = async (coin_id, currency_) => {
-        const { data } = await axios.get(`https://api.coingecko.com/api/v3/coins/${coin_id}/market_chart?vs_currency=${currency_}&days=30`);
+  const getData = async (coin_id, currency_) => {
+    const { data } = await axios.get(`https://api.coingecko.com/api/v3/coins/${coin_id}/market_chart?vs_currency=${currency_}&days=30`);
 
-        setChartData(data)
-        console.log(data)
-    }
+    setChartData(data.prices)
+    console.log(data.prices)
+  }
 
-    useEffect(() => {
-        getData(id, currency)
-    }, [id, currency])
+  useEffect(() => {
+    getData(id, currency)
+  }, [id, currency])
+
+  const Chart = () => {
+    ChartJS.register(LineElement, PointElement, LinearScale, Title);
+    return(
+      <Line data={chartData}/>
+    )
+  }
   return (
     <div>
-        <span id="coin-page-title">
+      <span id="coin-page-title">
         <h1>{id.toUpperCase()}</h1>
-        </span>
+      </span>
+      <Chart/>
     </div>
   )
 }
